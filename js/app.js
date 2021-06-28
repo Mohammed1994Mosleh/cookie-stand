772f97969915f4189fb45bd07eb52f0a7efd5752
Mohammed1994Mosleh <mohammed1994mosleh@gmail.com>
2021-06-28 23:36:26 +0300

    solve class07

--- js/app.js
+++ js/app.js
@@ -1,335 +1,265 @@
 'use strict';
 
-let seattle ={
-shopName:'seattle',    
- minCust:23,
- maxCust:65,
- avgC:6.3,
-  purchased:[],
+// let seattle ={
+// shopName:'seattle',    
+//  minCust:23,
+//  maxCust:65,
+//  avgC:6.3,
+//   purchased:[],
 
+// purchase: function(minCust,maxCust,avgC){
+// for(let i=0;i<14;i++){
+  
+//     this.purchased[i] = (Math.floor(Math.random() * (maxCust - minCust + 1) + minCust))*avgC;
+    
+//     }
 
-purchase: function(minCust,maxCust,avgC){
-for(let i=0;i<14;i++){
+// },
+
+// render:function(){
+   
+//     let total=0;
+//     let divEl = document.getElementById('seattle');
+//     let h2El = document.createElement('h2');
+//         divEl.appendChild(h2El);
+//         h2El.textContent = this.shopName;
+
+//         let ulEl = document.createElement('ul');
+//         for (let i = 0; i <= 14; i++) {
+            
+//             let b=6;
+            
+//             console.log(total);
+            
+//             let liEl = document.createElement('li');
+           
+            
+//            if (i<=5){
+            
+//             liEl.textContent =`${b+i}am:${this.purchased[i]}coockies` 
+//              }
+//              else if(i==6){
+
+//                 liEl.textContent =`12pm:${this.purchased[i]}coockies`
+            
+//              }  else if(i>6&&i<14){
+
+//                 liEl.textContent =`${i-6}pm:${this.purchased[i]}coockies`
+
+//              }       else{
+//                 liEl.textContent =`total:${total}cookies`
+                
+//              }
+            
+            
+//             ulEl.appendChild(liEl);
+//             total=total+this.purchased[i];
+//             }
+//             divEl.appendChild(ulEl);
+           
+//         }
+//     }
+
+// seattle.purchase(23,65,6.3);
+// seattle.render()
+let hours=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
+let charic=[];
+let totalPd=[];
+let sum2=0;
+
+function Stores (Name, minCust, maxCust, avgC) {
+    
+    this.Name=Name;
+    this.minCust=minCust;
+    this.maxCust=maxCust;
+    this.avgC=avgC;
+    
+    charic.push(this);
+    this.purchased=[];
+    this.sum=0;
+
+    
+}
+
+Stores.prototype.purchase=function(minCust,maxCust,avgC){
+    for(let i=0;i<14;i++){
   
     this.purchased[i] = (Math.floor(Math.random() * (maxCust - minCust + 1) + minCust))*avgC;
-    
+    this.sum+=this.purchased[i];
+
     }
+}
 
-},
+// let seattle=new Stores('seattle',12,20,6.5)
+// console.log(seattle);
 
-render:function(){
+// let contaIner =document.getElementById('con');
+// let table = document.createElement('table');
+// contaIner.appendChild(table);
+let contaIner =document.getElementById('con');
+let table = document.createElement('table');
+contaIner.appendChild(table);
+
+
+
+function toTalpd (){
+for (let i=0;i<hours.length;i++){
+
+sum2=seattle.purchased[i]+tokyo.purchased[i]+dubai.purchased[i]+paris.purchased[i]+lima.purchased[i];
+totalPd.push(sum2);
+// totalPd.push((seattle.purchased[i]+tokyo.purchased[i]+dubai.purchased[i]+paris.purchased[i]+lima.purchased[i]))
+
+}
+
+
+
+}
+
+
+function creatheader(){
+    
+let raws=document.createElement('tr')
+table.appendChild(raws);
+
+for(let i=0;i<=hours.length;i++){
+ let th;
+    if(i==0){
+        let th=document.createElement('th');
+        th.textContent=[   ];
+        raws.appendChild(th);
+
+    }
    
-    let total=0;
-    let divEl = document.getElementById('seattle');
-    let h2El = document.createElement('h2');
-        divEl.appendChild(h2El);
-        h2El.textContent = this.shopName;
 
-        let ulEl = document.createElement('ul');
-        for (let i = 0; i <= 14; i++) {
-            
-            let b=6;
-            
-            console.log(total);
-            
-            let liEl = document.createElement('li');
-           
-            
-           if (i<=5){
-            
-            liEl.textContent =`${b+i}am:${this.purchased[i]}coockies` 
-             }
-             else if(i==6){
-
-                liEl.textContent =`12pm:${this.purchased[i]}coockies`
-            
-             }  else if(i>6&&i<14){
-
-                liEl.textContent =`${i-6}pm:${this.purchased[i]}coockies`
+if (i<hours.length){
+    
+let th=document.createElement('th');
+th.textContent=hours[i];
+raws.appendChild(th);
+}
+if(i==hours.length){
+th=document.createElement('th');
+th.textContent='total';
+raws.appendChild(th);
 
 
-             }       else{
-                liEl.textContent =`total:${total}cookies`
-                
-             }
-            
-            
-            ulEl.appendChild(liEl);
-            total=total+this.purchased[i];
-            }
-            divEl.appendChild(ulEl);
-           
+}
+
+}
+
+}
+
+
+Stores.prototype.creaTetable=function(){
+
+    let raws=document.createElement('tr')
+    table.appendChild(raws);
+
+    for(let i=0;i<=(hours.length);i++){
+
+        if(i==0){
+            let td=document.createElement('td');
+            td.textContent=this.Name;
+            raws.appendChild(td);
+    
         }
+
+        if (i<hours.length){
+    let td=document.createElement('td');
+    td.textContent=this.purchased[i];
+    raws.appendChild(td);
+        }
+
+        if(i==hours.length){
+            let td=document.createElement('td');
+    td.textContent=this.sum;
+    raws.appendChild(td);
+
+        }
+       
+
+
+
+
+    
     }
 
-seattle.purchase(23,65,6.3);
-seattle.render()
+   
+   
+   
 
 
 
 
+}
+
+function lasTraw(){
+    let raws=document.createElement('tr')
+    table.appendChild(raws);
+
+for(let i=0;i<hours.length;i++){
+ 
 
 
-let Tokyo ={
-    shopName:'Tokyo',    
-     minCust:3,
-     maxCust:24,
-     avgC:1.2,
-      purchased:[],
     
-    
-    purchase: function(minCust,maxCust,avgC){
-    for(let i=0;i<14;i++){
-      
-        this.purchased[i] = (Math.floor(Math.random() * (maxCust - minCust + 1) + minCust))*avgC;
-        
-        }
-    
-    },
-    
-    render:function(){
-       
-        let total=0;
-        let divEl = document.getElementById(this.shopName);
-        let h2El = document.createElement('h2');
-            divEl.appendChild(h2El);
-            h2El.textContent = this.shopName;
-    
-            let ulEl = document.createElement('ul');
-            for (let i = 0; i <= 14; i++) {
-                
-                let b=6;
-                
-                console.log(total);
-                
-                let liEl = document.createElement('li');
-               
-                
-               if (i<=5){
-                
-                liEl.textContent =`${b+i}am:${this.purchased[i]}coockies` 
-                 }
-                 else if(i==6){
-    
-                    liEl.textContent =`12pm:${this.purchased[i]}coockies`
-                
-                 }  else if(i>6&&i<14){
-    
-                    liEl.textContent =`${i-6}pm:${this.purchased[i]}coockies`
-    
-    
-                 }       else{
-                    liEl.textContent =`total:${total}cookies`
-                    
-                 }
-                
-                
-                ulEl.appendChild(liEl);
-                total=total+this.purchased[i];
-                }
-                divEl.appendChild(ulEl);
-               
-            }
-        }
-    
-    Tokyo.purchase(3,24,1.2);
-    Tokyo.render()
-
-
-    let Dubai ={
-        shopName:'Dubai',    
-         minCust:11,
-         maxCust:38,
-         avgC:3.7,
-          purchased:[],
-        
-        
-        purchase: function(minCust,maxCust,avgC){
-        for(let i=0;i<14;i++){
-          
-            this.purchased[i] = (Math.floor(Math.random() * (maxCust - minCust + 1) + minCust))*avgC;
-            
-            }
-        
-        },
-        
-        render:function(){
-           
-            let total=0;
-            let divEl = document.getElementById(this.shopName);
-            let h2El = document.createElement('h2');
-                divEl.appendChild(h2El);
-                h2El.textContent = this.shopName;
-        
-                let ulEl = document.createElement('ul');
-                for (let i = 0; i <= 14; i++) {
-                    
-                    let b=6;
-                    
-                    console.log(total);
-                    
-                    let liEl = document.createElement('li');
-                   
-                    
-                   if (i<=5){
-                    
-                    liEl.textContent =`${b+i}am:${this.purchased[i]}coockies` 
-                     }
-                     else if(i==6){
-        
-                        liEl.textContent =`12pm:${this.purchased[i]}coockies`
-                    
-                     }  else if(i>6&&i<14){
-        
-                        liEl.textContent =`${i-6}pm:${this.purchased[i]}coockies`
-        
-        
-                     }       else{
-                        liEl.textContent =`total:${total}cookies`
-                        
-                     }
-                    
-                    
-                    ulEl.appendChild(liEl);
-                    total=total+this.purchased[i];
-                    }
-                    divEl.appendChild(ulEl);
-                   
-                }
-            }
-        
-            Dubai.purchase(11,38,3.7);
-            Dubai.render()
-
-
-            let paris ={
-                shopName:'paris',    
-                 minCust:20,
-                 maxCust:38,
-                 avgC:2.3,
-                  purchased:[],
-                
-                
-                purchase: function(minCust,maxCust,avgC){
-                for(let i=0;i<14;i++){
-                  
-                    this.purchased[i] = (Math.floor(Math.random() * (maxCust - minCust + 1) + minCust))*avgC;
-                    
-                    }
-                
-                },
-                
-                render:function(){
-                   
-                    let total=0;
-                    let divEl = document.getElementById(this.shopName);
-                    let h2El = document.createElement('h2');
-                        divEl.appendChild(h2El);
-                        h2El.textContent = this.shopName;
-                
-                        let ulEl = document.createElement('ul');
-                        for (let i = 0; i <= 14; i++) {
-                            
-                            let b=6;
-                            
-                            console.log(total);
-                            
-                            let liEl = document.createElement('li');
-                           
-                            
-                           if (i<=5){
-                            
-                            liEl.textContent =`${b+i}am:${this.purchased[i]}coockies` 
-                             }
-                             else if(i==6){
-                
-                                liEl.textContent =`12pm:${this.purchased[i]}coockies`
-                            
-                             }  else if(i>6&&i<14){
-                
-                                liEl.textContent =`${i-6}pm:${this.purchased[i]}coockies`
-                
-                
-                             }       else{
-                                liEl.textContent =`total:${total}cookies`
-                                
-                             }
-                            
-                            
-                            ulEl.appendChild(liEl);
-                            total=total+this.purchased[i];
-                            }
-                            divEl.appendChild(ulEl);
-                           
-                        }
-                    }
-                
-                    paris.purchase(20,38,2.3);
-                    paris.render()
+    if(i==0){
+        let td=document.createElement('td');
+        td.textContent='total';
+        raws.appendChild(td);
 
 
 
-                    let lima ={
-                        shopName:'lima',    
-                         minCust:2,
-                         maxCust:16,
-                         avgC:4.6,
-                          purchased:[],
-                        
-                        
-                        purchase: function(minCust,maxCust,avgC){
-                        for(let i=0;i<14;i++){
-                          
-                            this.purchased[i] = (Math.floor(Math.random() * (maxCust - minCust + 1) + minCust))*avgC;
-                            
-                            }
-                        
-                        },
-                        
-                        render:function(){
-                           
-                            let total=0;
-                            let divEl = document.getElementById(this.shopName);
-                            let h2El = document.createElement('h2');
-                                divEl.appendChild(h2El);
-                                h2El.textContent = this.shopName;
-                        
-                                let ulEl = document.createElement('ul');
-                                for (let i = 0; i <= 14; i++) {
-                                    
-                                    let b=6;
-                                    
-                                    console.log(total);
-                                    
-                                    let liEl = document.createElement('li');
-                                   
-                                    
-                                   if (i<=5){
-                                    
-                                    liEl.textContent =`${b+i}am:${this.purchased[i]}coockies` 
-                                     }
-                                     else if(i==6){
-                        
-                                        liEl.textContent =`12pm:${this.purchased[i]}coockies`
-                                    
-                                     }  else if(i>6&&i<14){
-                        
-                                        liEl.textContent =`${i-6}pm:${this.purchased[i]}coockies`
-                        
-                        
-                                     }       else{
-                                        liEl.textContent =`total:${total}cookies`
-                                        
-                                     }
-                                    
-                                    
-                                    ulEl.appendChild(liEl);
-                                    total=total+this.purchased[i];
-                                    }
-                                    divEl.appendChild(ulEl);
-                                   
-                                }
-                            }
-                        
-                            lima.purchase(2,16,4.6);
-                            lima.render()
+    }
+    let td=document.createElement('td');
+    td.textContent=totalPd[i];
+    raws.appendChild(td);
+
+
+
+}
+
+
+
+
+
+
+}
+
+
+
+
+creatheader();
+let seattle=new Stores('seattle',12,60,6.5);
+
+seattle.purchase(12,5,6.5);
+
+seattle.creaTetable();
+
+let tokyo=new Stores('tokyo',2,3,4);
+tokyo.purchase(2,3,4.5);
+tokyo.creaTetable();
+
+let dubai=new Stores('dubai',20,60,2.3);
+dubai.purchase(20,60,2.3);
+dubai.creaTetable();
+
+let paris=new Stores('paris',70,80,3);
+paris.purchase(70,80,3);
+paris.creaTetable();
+
+let lima=new Stores('lima',20,30,5);
+lima.purchase(20,30,5);
+lima.creaTetable();
+
+toTalpd();
+console.log();
+lasTraw();
+console.log(totalPd);
+
+
+
+
+
+
+console.log(seattle.purchased);
